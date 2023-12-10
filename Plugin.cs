eee830fd4f1dda69b9aa840cd3e26cc99a882305
MatthewZelriche <67518084+MatthewZelriche@users.noreply.github.com>
2023-12-10 17:53:56 -0600

    Initial commit

---
+++ Plugin.cs
@@ -0,0 +1,19 @@
+#if TOOLS
+using Godot;
+using System;
+
+[Tool]
+public partial class Plugin : EditorPlugin
+{
+    public override void _EnterTree()
+    {
+        // Initialization of the plugin goes here.
+        GD.Print("Hello, world!");
+    }
+
+    public override void _ExitTree()
+    {
+        // Clean-up of the plugin goes here.
+    }
+}
+#endif
