c75b5aaa12cd5e7cc93394e3bffaa73b6e610b08
MatthewZelriche <67518084+MatthewZelriche@users.noreply.github.com>
2023-12-11 14:20:17 -0600

    Implement basic camera class

--- src/TRCamera.cs
+++ src/TRCamera.cs
@@ -2,13 +2,62 @@ using Godot;
 
 public partial class TRCamera : Camera3D
 {
-    // Called when the node enters the scene tree for the first time.
+    [Export]
+    // Corresponds to the cvar 'm_yaw' in goldSrc
+    private float yawSpeed = 0.022f;
+    [Export]
+    // Corresponds to the cvar 'm_pitch' in goldSrc
+    private float pitchSpeed = 0.022f;
+    [Export]
+    // Corresponds to the cvar 'sensitivity' in goldSrc
+    private float sensitivity = 2.0f;
+
+    private float pitch;
+    private float yaw;
+
     public override void _Ready()
     {
+        // Camera may start rotated - so we need to get the "starting" pitch and yaw 
+        pitch = Mathf.RadToDeg(Rotation.X);
+        yaw = Mathf.RadToDeg(Rotation.Y);
     }
 
-    // Called every frame. 'delta' is the elapsed time since the previous frame.
-    public override void _Process(double delta)
+    public Vector3 UnitForwardHorzVector()
     {
+        // Project the Forward Vector onto the XZ horizontal plane, so that we
+        // can get the player's horizontal movement direction irrespective of their 
+        // camera pitch
+        return new Plane(new Vector3(0, 1, 0)).Project(UnitForwardVector()).Normalized();
+    }
+
+    public Vector3 UnitForwardVector()
+    {
+        return -GlobalTransform.Basis.Z.Normalized();
+    }
+
+    public Vector3 UnitRightVector()
+    {
+        return GlobalTransform.Basis.X.Normalized();
+    }
+
+    public override void _Input(InputEvent @event)
+    {
+        if (@event is InputEventMouseMotion motionEvent)
+        {
+            pitch -= motionEvent.Relative.Y * pitchSpeed * sensitivity;
+            yaw -= motionEvent.Relative.X * yawSpeed * sensitivity;
+
+            // Clamp camera to avoid flipping
+            if (pitch >= 89.0f)
+            {
+                pitch = 89.0f;
+            }
+            if (pitch <= -89.0f)
+            {
+                pitch = -89.0f;
+            }
+
+            Rotation = new Vector3(Mathf.DegToRad(pitch), Mathf.DegToRad(yaw), Rotation.Z);
+        }
     }
 }
