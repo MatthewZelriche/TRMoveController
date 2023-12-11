dee9e3f433c23862698dd883ed4eaac2b8a4413d
MatthewZelriche <67518084+MatthewZelriche@users.noreply.github.com>
2023-12-11 10:20:35 -0600

    Bsic scene setup

--- src/TRMoveController.cs
+++ src/TRMoveController.cs
@@ -1,16 +1,21 @@
 using Godot;
-using System;
+using System.Diagnostics;
 
 [GlobalClass]
 public partial class TRMoveController : RigidBody3D
 {
-    // Called when the node enters the scene tree for the first time.
-    public override void _Ready()
-    {
-    }
 
-    // Called every frame. 'delta' is the elapsed time since the previous frame.
-    public override void _Process(double delta)
-    {
-    }
+	[Export]
+	private Camera3D playerCamera = null;
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		Debug.Assert(playerCamera != null, "You forgot to assign a Camera3D to the TRMoveController!");
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
 }
